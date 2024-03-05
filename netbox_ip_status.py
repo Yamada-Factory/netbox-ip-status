95fce6ca46664f2456fb95d8b5762ce46db99ca8
NAKNAO-nnct <nakanoyuki@team-lab.com>
2024-03-05 17:06:45 +0900

    fix: reserved の時を考慮

--- netbox_ip_status.py
+++ netbox_ip_status.py
@@ -48,15 +48,15 @@ def update_address(ipy_address, prefix_mask = "24"):
 
         if address is not None:
             if ping_result.is_alive:
-                # MEMO: deprecated の時に ping が通るようになった場合、status を active に戻す
-                if address.status == 'deprecated':
+                # MEMO: deprecated / reserved の時に ping が通るようになった場合、status を active に戻す
+                if address.status in ['deprecated', 'reserved']:
                     address.status = 'active'
                     address.description = 'Updated at ' + today + '. ' + address.description
 
                     updated = True
             else:
-                # address が登録されてて、かつ ping が通らないときかつ status が deprecated 以外のとき
-                if address.status != 'deprecated':
+                # address が登録されてて、かつ ping が通らないときかつ status が deprecated or reserved 以外のとき
+                if address.status not in ['deprecated', 'reserved']:
                     address.status = 'deprecated'
                     address.description = 'Updated at ' + today + '. ' + address.description
 
