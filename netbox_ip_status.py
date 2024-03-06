7e9a1cfa7b4a14a8ed661ff97b691bc4ad4ce0a3
NAKNAO-nnct <totoro.windows@outlook.com>
2024-03-06 22:50:20 +0900

    update: descriptionからcommentsを更新するよう修正

--- netbox_ip_status.py
+++ netbox_ip_status.py
@@ -54,14 +54,14 @@ def update_address(ipy_address, prefix_mask = "24"):
                 # MEMO: deprecated / reserved の時に ping が通るようになった場合、status を active に戻す
                 if address.status.value in {'deprecated', 'reserved'}:
                     address.status = 'active'
-                    address.description = 'Updated at ' + today + '. ' + address.description
+                    address.comments = 'Updated at ' + today + '.\n' + address.description
 
                     updated = True
             else:
                 # address が登録されてて、かつ ping が通らないときかつ status が deprecated or reserved 以外のとき
                 if address.status.value not in {'deprecated', 'reserved'}:
                     address.status = 'deprecated'
-                    address.description = 'Updated at ' + today + '. ' + address.description
+                    address.comments = 'Updated at ' + today + '.\n' + address.description
 
                     updated = True
 
